a7a582a1d2d550be98c29267ebdf271f51875110
Documenter.jl <documenter@juliadocs.github.io>
2024-07-19 10:06:10 +0000

    build based on 8e840fc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Peregrin","page":"Peregrin","title":"Peregrin","text":"","category":"section"},{"location":"","page":"Peregrin","title":"Peregrin","text":"Documentation for Peregrin","category":"page"}]
+}
